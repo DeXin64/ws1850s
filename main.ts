40c64e43a7918fc628607e21add528c5ef25218e
can <canvnac@gmail.com>
2023-07-07 23:00:16 +0800

    124

---
+++ main.ts
@@ -0,0 +1,20 @@
+
+
+//% weight=10000 color=#357DF8 icon="\uf3fd" block="特萌扩展"
+//% groups=['NFC【IIC】', '其他']
+namespace temeng {
+
+    //% group="NFC【IIC】"
+    export function lightIIC(): string {
+        // 119 120
+        let SCL = 119
+        let SDL = 120
+        pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
+        basic.pause(1000);
+        pins.setPull(DigitalPin.P20, PinPullMode.PullDown)
+        return "hello!"
+    }
+
+}
+
+
