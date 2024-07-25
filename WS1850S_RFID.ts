563b07a83bc61a49788719a68a6888b75993def9
can <canvnac@gmail.com>
2024-07-25 22:37:31 +0800

    RFID

--- WS1850S_RFID.ts
+++ WS1850S_RFID.ts
@@ -171,6 +171,45 @@ namespace WS1850S_RFID {
         ClearBits(TxControlReg, 0x03) // 0x14控制天线驱动器管脚TX1和TX2的寄存器
     }
 
+    //% weight=90 block="读取卡ID"
+    //% group="读取卡数据"
+    //% color=#4B974A
+    export function readID(): number {
+        [status, Type2] = Request(PICC_REQIDL)  //寻卡+复位应答
+
+        if (status != 0) {
+            return null
+        }
+        [status, uid] = AvoidColl()
+
+        if (status != 0) {
+            return null
+        }
+
+        return getIDNum(uid)
+    }
+    //% weight=90 block="读取卡内部数据"
+    //% group="读取卡数据"
+    //% color=#4B974A
+    export function readData(): string {
+        return ReadData_manage()
+    }
+
+    //% weight=90 block="写入数据到卡内部|数据内容%data"
+    //% group="写入卡数据"
+    //% color=#4B974A
+    export function writeData(data: string) {
+        let id = writeToCard(data)
+
+        while (!id) {
+            let id = writeToCard(data)
+
+            if (id != undefined) {
+                break
+            }
+        }
+    }
+
     //% weight=90 block="版本2"
     //% group="公共"
     //% color=#4B974A
@@ -219,6 +258,349 @@ namespace WS1850S_RFID {
     function ClearBits(reg: number, mask: number) {
         let tmp = IIC_Read(reg)
         IIC_Write(reg, tmp & (~mask))
+    }
+
+    // readID的第一个函数----寻卡函数
+    function Request(reqMode: number): [number, number] {
+        let Type: number[] = []
+        IIC_Write(BitFramingReg, 0x07)  //0x0d面向位的帧的调节寄存器，0x07
+        Type.push(reqMode)
+        let [status, returnData, returnBits] = MFRC522_ToCard(PCD_TRANSCEIVE, Type)
+
+        if ((status != 0) || (returnBits != 16)) {
+            status = 2
+        }
+
+        return [status, returnBits]
+    }
+    function MFRC522_ToCard(command: number, sendData: number[]): [number, number[], number] {
+        returnData = []
+        returnLen = 0
+        status = 2
+        let irqEN = 0x00
+        let waitIRQ = 0x00
+        let lastBits = null
+        let n = 0
+    
+        if (command == PCD_AUTHENT) {
+            irqEN = 0x12
+            waitIRQ = 0x10
+        }
+    
+        if (command == PCD_TRANSCEIVE) {
+            irqEN = 0x77
+            waitIRQ = 0x30
+        }
+    
+        IIC_Write(0x02, irqEN | 0x80)
+        ClearBits(CommIrqReg, 0x80)
+        SetBits(FIFOLevelReg, 0x80)
+        IIC_Write(CommandReg, PCD_IDLE)
+    
+        for (let o = 0; o < (sendData.length); o++) {
+            IIC_Write(FIFODataReg, sendData[o])
+        }
+        IIC_Write(CommandReg, command)
+    
+        if (command == PCD_TRANSCEIVE) {
+            SetBits(BitFramingReg, 0x80)
+        }
+    
+        let p = 2000
+        while (true) {
+            n = IIC_Read(CommIrqReg)
+            p--
+            if (~(p != 0 && ~(n & 0x01) && ~(n & waitIRQ))) {
+                break
+            }
+        }
+        ClearBits(BitFramingReg, 0x80)
+    
+        if (p != 0) {
+            if ((IIC_Read(0x06) & 0x1B) == 0x00) {
+                status = 0
+                if (n & irqEN & 0x01) {
+                    status = 1
+                }
+                if (command == PCD_TRANSCEIVE) {
+                    n = IIC_Read(FIFOLevelReg)
+                    lastBits = IIC_Read(ControlReg) & 0x07
+                    if (lastBits != 0) {
+                        returnLen = (n - 1) * 8 + lastBits
+                    }
+                    else {
+                        returnLen = n * 8
+                    }
+                    if (n == 0) {
+                        n = 1
+                    }
+                    if (n > MAX_LEN) {
+                        n = MAX_LEN
+                    }
+                    for (let q = 0; q < n; q++) {
+                        returnData.push(IIC_Read(FIFODataReg))
+                    }
+                }
+            }
+            else {
+                status = 2
+            }
+        }
+    
+        return [status, returnData, returnLen]
+    }
+    // readID的第二个函数
+    function AvoidColl(): [number, number[]] {
+        let SerNum = []
+        ChkSerNum = 0
+        IIC_Write(BitFramingReg, 0)
+        SerNum.push(PICC_ANTICOLL)
+        SerNum.push(0x20)
+        let [status, returnData, returnBits] = MFRC522_ToCard(PCD_TRANSCEIVE, SerNum)
+
+        if (status == 0) {
+            if (returnData.length == 5) {
+                for (let k = 0; k <= 3; k++) {
+                    ChkSerNum = ChkSerNum ^ returnData[k]
+                }
+                if (ChkSerNum != returnData[4]) {
+                    status = 2
+                }
+            }
+            else {
+                status = 2
+            }
+        }
+        return [status, returnData]
+    }
+    // readID的第三个函数
+    function getIDNum(uid: number[]) {
+        let a = 0
+
+        for (let e = 0; e < 5; e++) {
+            a = a * 256 + uid[e]
+        }
+        return a
+    }
+
+    // 读取的卡数据read()处理函数
+    // 由于卡2扇区数据共48个字节（即48个字符），填入数据后，未填写的数据会自动补字符0（即16进制的0X20）
+    // 这将导致海客LCD显示屏，显示的字符重叠，导致显示故障 
+    // 所以需要编写一个仅显示有效字符串，去除补位字符0
+    function ReadData_manage(): string {
+        let manage_DATA: string
+        let m_DATA_1: string = read()
+        manage_DATA = m_DATA_1.trim()   //.trim()为js语言去除两端空格的函数
+        return manage_DATA
+    }
+    // Read读取卡M1卡数据主函数
+    function read(): string {               //数据长度48个字节
+        let text = readFromCard()
+        while (!text) {
+            let text = readFromCard()
+
+            if (text != '') {
+                return text
+            }
+        }
+        return text
+    }
+    // Read读取卡M1卡数据第一个函数
+    function readFromCard(): string {
+        let [status, Type2] = Request(PICC_REQIDL)     //寻卡+复位应答
+        if (status != 0) {
+            return null
+        }
+
+        [status, uid] = AvoidColl()     //防多卡冲突机制
+
+        if (status != 0) {
+            return null
+        }
+
+        let id = getIDNum(uid)
+        TagSelect(uid)                  //选择卡片
+        status = Authent(PICC_AUTHENT1A, 11, Key, uid)  //三次相互验证
+        let data: NumberFormat.UInt8LE[] = []
+        let text_read = ''
+        let block: number[] = []
+        if (status == 0) {
+            for (let BlockNum of BlockAdr) {
+                block = ReadRFID(BlockNum)
+                if (block) {
+                    data = data.concat(block)
+                }
+            }
+            if (data) {
+                for (let c of data) {
+                    text_read = text_read.concat(String.fromCharCode(c))
+                }
+            }
+        }
+        Crypto1Stop()
+        return text_read
+    }
+    // Read读取卡M1卡数据第二个函数
+    function TagSelect(SerNum: number[]) {
+        let buff: number[] = []
+        buff.push(0x93)
+        buff.push(0x70)
+        for (let r = 0; r < 5; r++) {
+            buff.push(SerNum[r])
+        }
+
+        let pOut = CRC_Calculation(buff)
+        buff.push(pOut[0])
+        buff.push(pOut[1])
+        let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, buff)
+        if ((status == 0) && (returnLen == 0x18)) {
+            return returnData[0]
+        }
+        else {
+            return 0
+        }
+    }
+    // Read读取卡M1卡数据第三个函数
+    function Authent(authMode: number, BlockAdr: number, Sectorkey: number[], SerNum: number[]): number {
+        let buff: number[] = []
+        buff.push(authMode)
+        buff.push(BlockAdr)
+        for (let l = 0; l < (Sectorkey.length); l++) {
+            buff.push(Sectorkey[l])
+        }
+        for (let m = 0; m < 4; m++) {
+            buff.push(SerNum[m])
+        }
+        [status, returnData, returnLen] = MFRC522_ToCard(PCD_AUTHENT, buff)
+        if (status != 0) {
+            serial.writeLine("AUTH ERROR!")
+        }
+        if ((IIC_Read(Status2Reg) & 0x08) == 0) {
+            serial.writeLine("AUTH ERROR2!")
+        }
+        return status
+    }
+    // Read读取卡M1卡数据第四个函数
+    function ReadRFID(blockAdr: number): number[] {
+        recvData = []
+        recvData.push(PICC_READ)
+        recvData.push(blockAdr)
+        let pOut2 = []
+        pOut2 = CRC_Calculation(recvData)
+        recvData.push(pOut2[0])
+        recvData.push(pOut2[1])
+        let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, recvData)
+
+        if (status != 0) {
+            serial.writeLine("Error while reading!")
+        }
+
+        if (returnData.length != 16) {
+            return null
+        }
+        else {
+            return returnData
+        }
+    }
+    //---------Read读取卡M1卡数据第五个函数------------------------//
+    function Crypto1Stop() {
+        ClearBits(Status2Reg, 0x08)
+    }
+    // Read读取卡M1卡数据第六个函数
+    function CRC_Calculation(DataIn: number[]) {
+        ClearBits(DivIrqReg, 0x04)
+        SetBits(FIFOLevelReg, 0x80)
+        for (let s = 0; s < (DataIn.length); s++) {
+            IIC_Write(FIFODataReg, DataIn[s])
+        }
+        IIC_Write(CommandReg, 0x03)
+        let t = 0xFF
+
+        while (true) {
+            let v = IIC_Read(DivIrqReg)
+            t--
+            if (!(t != 0 && !(v & 0x04))) {
+                break
+            }
+        }
+
+        let DataOut: number[] = []
+        DataOut.push(IIC_Read(0x22))
+        DataOut.push(IIC_Read(0x21))
+        return DataOut
+    }
+
+    // write卡数据的第一个函数
+    function writeToCard(txt: string): number {
+        [status, Type2] = Request(PICC_REQIDL)
+
+        if (status != 0) {
+            return null
+        }
+        [status, uid] = AvoidColl()
+
+        if (status != 0) {
+            return null
+        }
+
+        let id = getIDNum(uid)
+        TagSelect(uid)
+        status = Authent(PICC_AUTHENT1A, 11, Key, uid)
+        ReadRFID(11)
+
+        if (status == 0) {
+            let data: NumberFormat.UInt8LE[] = []
+            for (let i = 0; i < txt.length; i++) {
+                data.push(txt.charCodeAt(i))
+            }
+
+            for (let j = txt.length; j < 48; j++) {
+                data.push(32)
+            }
+
+            let b = 0
+            for (let BlockNum2 of BlockAdr) {
+                WriteRFID(BlockNum2, data.slice((b * 16), ((b + 1) * 16)))
+                b++
+            }
+        }
+
+        Crypto1Stop()
+        serial.writeLine("Written to Card")
+        return id
+    }
+    // write卡数据的第二个函数
+    function WriteRFID(blockAdr: number, writeData: number[]) {
+        let buff: number[] = []
+        let crc: number[] = []
+
+        buff.push(0xA0)
+        buff.push(blockAdr)
+        crc = CRC_Calculation(buff)
+        buff.push(crc[0])
+        buff.push(crc[1])
+        let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, buff)
+        if ((status != 0) || (returnLen != 4) || ((returnData[0] & 0x0F) != 0x0A)) {
+            status = 2
+            serial.writeLine("ERROR")
+        }
+
+        if (status == 0) {
+            let buff2: number[] = []
+            for (let w = 0; w < 16; w++) {
+                buff2.push(writeData[w])
+            }
+            crc = CRC_Calculation(buff2)
+            buff2.push(crc[0])
+            buff2.push(crc[1])
+            let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, buff2)
+            if ((status != 0) || (returnLen != 4) || ((returnData[0] & 0x0F) != 0x0A)) {
+                serial.writeLine("Error while writing")
+            }
+            else {
+                serial.writeLine("Data written")
+            }
+        }
     }
 
     function readId2(): [number, string] {
